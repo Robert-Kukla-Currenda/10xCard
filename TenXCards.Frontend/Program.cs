7dbc01d1c675057bf7fbbf2000223d545670f7db
Robert Kukla <robert.kukla@currenda.pl>
2025-04-21 19:14:18 +0200

    refactor: Refactor navigation menu and remove unused Counter and Weather pages

--- TenXCards.Frontend/Program.cs
+++ TenXCards.Frontend/Program.cs
@@ -1,10 +1,25 @@
 using TenXCards.Frontend.Components;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//Add MudBlazor services
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = true;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 5000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+});
 
 var app = builder.Build();
 
@@ -17,11 +32,10 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-
+//app.UseStaticFiles();
+app.MapStaticAssets();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
